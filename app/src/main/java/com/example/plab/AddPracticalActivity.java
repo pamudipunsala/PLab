8c95df15a5b7f50d822181945bd8d1884f5355f2
Hansaka Eranda <87437151+HansakaEranda@users.noreply.github.com>
2022-11-06 01:41:06 +0530

    Add Practicals backend

--- app/src/main/java/com/example/plab/AddPracticalActivity.java
+++ app/src/main/java/com/example/plab/AddPracticalActivity.java
@@ -3,12 +3,32 @@ package com.example.plab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddPracticalActivity extends AppCompatActivity {
+
+    EditText title_input, lesson_input, steps_input;
+    Button add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_practical2);
+
+        title_input = findViewById(R.id.title_input);
+        lesson_input = findViewById(R.id.lesson_input);
+        steps_input = findViewById(R.id.steps_input);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddPracticalActivity.this);
+                myDB.addPractical(title_input.getText().toString().trim(),
+                        lesson_input.getText().toString().trim(),
+                        steps_input.getText().toString().trim());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/plab/MyDatabaseHelper.java
+++ app/src/main/java/com/example/plab/MyDatabaseHelper.java
@@ -1,8 +1,10 @@
 package com.example.plab;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -29,7 +31,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
                         " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                         COLUMN_TITLE + " TEXT, " +
                         COLUMN_LESSON + " TEXT, " +
-                        COLUMN_STEPS + " INTEGER);";
+                        COLUMN_STEPS + " TEXT);";
         db.execSQL(query);
     }
 
@@ -37,5 +39,20 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    void addPractical(String title, String lesson, String steps) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_LESSON, lesson);
+        cv.put(COLUMN_STEPS, steps);
+        long result = db.insert(TABLE_NAME, null, cv);
+        if(result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context, "Added Successfully", Toast.LENGTH_SHORT).show();
+        }
     }
 }
