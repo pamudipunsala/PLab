0fc675d40c8d3377c13be9e3310fd27b40b1845f
HasiThish <86554418+HasiThish@users.noreply.github.com>
2022-11-11 14:53:59 +0530

    Add quiz files

---
+++ app/src/main/java/com/example/plab/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.plab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
